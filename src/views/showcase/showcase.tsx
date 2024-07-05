2b9dea946023dd073d77e184e180300357ab743f
Tyler <54653079+CyberProgrammer@users.noreply.github.com>
2024-07-05 19:21:41 -0400

    Made it more mobile friendly and finished the projects layout.

--- src/views/showcase/showcase.tsx
+++ src/views/showcase/showcase.tsx
@@ -1,3 +1,11 @@
+import './showcase.css'
+import './card.css'
+
+import ConnectPreview from '@assets/images/cards/connect.jpg'
+import TaskTeamPreview from '@assets/images/cards/task-team.png';
+import ACSimulatorPreview from '@assets/images/cards/ac-sim.png'
+
+import LinkIcon from '@assets/icons/web-link.svg';
 
 const Showcase = () => {
 
@@ -8,8 +16,40 @@ const Showcase = () => {
             </div>
             <div className={"card-container"}>
                 <div className={"card"}>
-                    <h2>Title</h2>
-                    <p>SHort description</p>
+                    <div className={"card-header"}>
+                        <h2 className={"card-title"}>Connect Messenger</h2>
+                        <button className={"card-button"}>
+                            <p>Live Demo</p>
+                            <img className={"link-img"} src={LinkIcon} alt={"icon"}/>
+                        </button>
+                    </div>
+                    <p className={"card-description"}>This is an example of a short description.
+                        The purpose is to demonstrate what the capabilities and main objective is.</p>
+                    <img className={"card-img"} src={ConnectPreview} alt={"img"}/>
+                </div>
+                <div className={"card"}>
+                    <div className={"card-header"}>
+                        <h2 className={"card-title"}>Task Team</h2>
+                        <button className={"card-button"}>
+                            <p>Live Demo</p>
+                            <img className={"link-img"} src={LinkIcon} alt={"icon"}/>
+                        </button>
+                    </div>
+                    <p className={"card-description"}>This is an example of a short description.
+                        The purpose is to demonstrate what the capabilities and main objective is.</p>
+                    <img className={"card-img"} src={TaskTeamPreview} alt={"img"}/>
+                </div>
+                <div className={"card"}>
+                    <div className={"card-header"}>
+                        <h2 className={"card-title"}>A/C Simulation</h2>
+                        <button className={"card-button"}>
+                            <p>Live Demo</p>
+                            <img className={"link-img"} src={LinkIcon} alt={"icon"}/>
+                        </button>
+                    </div>
+                    <p className={"card-description"}>This is an example of a short description.
+                        The purpose is to demonstrate what the capabilities and main objective is.</p>
+                    <img className={"card-img"} src={ACSimulatorPreview} alt={"img"}/>
                 </div>
             </div>
         </>
